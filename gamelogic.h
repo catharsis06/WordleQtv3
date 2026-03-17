92a77777b00784d96bb122f88b955e11134b9561
catharsis06 <max_2_06@mail.ru>
2026-03-17 22:44:51 +0300

    feat: добавлена реализация заголовочного файла GameLogic

--- gamelogic.h
+++ gamelogic.h
@@ -8,9 +8,21 @@ class GameLogic : public QObject
     Q_OBJECT
 public:
     explicit GameLogic(QObject *parent = nullptr);
+    void startNewGame();
+    QString processGuess(const QString &guess);
+    int getScore() const;
+    bool isGameOver() const;
+    void saveScore();
 
 signals:
+    void logMessage(const QString &text);
 
+private:
+    QString m_secretWord;
+    int m_attempts;
+    int m_score;
+
+    QString getFeedback(const QString &guess) const;
 };
 
 #endif // GAMELOGIC_H
