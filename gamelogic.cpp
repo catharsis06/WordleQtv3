44db8f592de9976e59aad6330564e4fad7dae944
catharsis06 <max_2_06@mail.ru>
2026-03-18 21:10:26 +0300

    feat: добавить конструктор GameLogic

--- gamelogic.cpp
+++ gamelogic.cpp
@@ -1,6 +1,9 @@
 #include "gamelogic.h"
+#include <QTime>
 
-GameLogic::GameLogic(QObject *parent) : QObject(parent)
+GameLogic::GameLogic(QObject *parent) : QObject(parent), m_attempts(0), m_score (0)
 {
+    qsrand(QTime::currentTime().msecsSinceStartOfDay());
 
+    startNewGame();
 }
