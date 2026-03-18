2787f6cf87bf6fb676325a0cb9a9594f5ca4c061
catharsis06 <max_2_06@mail.ru>
2026-03-18 21:18:06 +0300

    feat: добавить startNewGame

--- gamelogic.cpp
+++ gamelogic.cpp
@@ -1,9 +1,27 @@
 #include "gamelogic.h"
 #include <QTime>
+#include <QFile>
+#include <QTextStream>
+#include <QDir>
 
 GameLogic::GameLogic(QObject *parent) : QObject(parent), m_attempts(0), m_score (0)
 {
     qsrand(QTime::currentTime().msecsSinceStartOfDay());
 
     startNewGame();
+}
+
+void GameLogic::startNewGame()
+{
+    const QString words[10] = {
+            "барс", "рука", "лось", "перо", "нога",
+            "стол", "окно", "сова", "тигр", "рыба"
+        };
+
+    m_secretWord = words[qrand()%10];
+
+    m_attempts = 0;
+    m_score = 0;
+
+    emit logMessage("[game]: Загаданное слово: ****");
 }
